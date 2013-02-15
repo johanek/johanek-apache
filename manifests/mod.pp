ea1112c3d9507a67dc0555e3deb4504e673f26be
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-02-15 21:03:21 +0000

    Remove strange 2.6 compatibility hacks

--- manifests/mod.pp
+++ manifests/mod.pp
@@ -3,36 +3,23 @@ define apache::mod (
 ) {
   $mod = $name
   $mod_packages = $apache::params::mod_packages
-  $mod_package = $mod_packages[$mod] # 2.6 compatibility hack
-  if $package {
-    $package_REAL = $package
-  } elsif $mod_package {
-    $package_REAL = $mod_package
-  }
+
   $mod_libs = $apache::params::mod_libs
-  $mod_lib = $mod_libs[$mod] # 2.6 compatibility hack
-  if $mod_lib {
-    $lib = $mod_lib
-  }
 
   $mod_identifiers = $apache::params::mod_identifiers
-  $mod_identifier = $mod_identifiers[$mod]
-  if $mod_identifier {
-    $identifier = $mod_identifier
-  }
 
-  if $package_REAL {
-    package { $package_REAL:
+  if $package {
+    package { "$package":
       ensure   => present,
       require  => Class['apache::install'],
-      before   => A2mod[$mod],
+      before   => A2mod[$mod]
     }
   }
 
   a2mod { $mod:
     ensure     => present,
-    lib        => $lib,
-    identifier => $identifier,
+    lib        => $mod_libs,
+    identifier => $mod_identifiers,
     require    => Class['apache::install'],
     notify     => Class['apache::service']
   }
