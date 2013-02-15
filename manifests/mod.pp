ce66d00ba8b9d148e19d1b7e4f5fdfc9b9dd4bd2
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-02-15 21:25:27 +0000

    I obviously have no idea what I'm doing.

--- manifests/mod.pp
+++ manifests/mod.pp
@@ -3,23 +3,36 @@ define apache::mod (
 ) {
   $mod = $name
   $mod_packages = $apache::params::mod_packages
-
+  $mod_package = $mod_packages[$mod] # 2.6 compatibility hack
+  if $package {
+    $package_REAL = $package
+  } elsif "$mod_package" {
+    $package_REAL = $mod_package
+  }
   $mod_libs = $apache::params::mod_libs
+  $mod_lib = $mod_libs[$mod] # 2.6 compatibility hack
+  if "${mod_lib}" {
+    $lib = $mod_lib
+  }
 
   $mod_identifiers = $apache::params::mod_identifiers
+  $mod_identifier = $mod_identifiers[$mod]
+  if "${mod_identifier}" {
+    $identifier = $mod_identifier
+  }
 
-  if $package {
-    package { "$package":
+  if $package_REAL {
+    package { $package_REAL:
       ensure   => present,
       require  => Class['apache::install'],
-      before   => A2mod[$mod]
+      before   => A2mod[$mod],
     }
   }
 
   a2mod { $mod:
     ensure     => present,
-    lib        => $mod_libs,
-    identifier => $mod_identifiers,
+    lib        => $lib,
+    identifier => $identifier,
     require    => Class['apache::install'],
     notify     => Class['apache::service']
   }
