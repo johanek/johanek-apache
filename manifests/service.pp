26ff7bac298704d569344853c2e6cdc7ac7845c5
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-02-15 18:42:15 +0000

    Fix alignment

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,6 @@ class apache::service {
   service { 'httpd':
     ensure  => $apache::params::service_ensure,
     enable  => $apache::params::service_enable,
-    name      => $apache::params::apache_name,
+    name    => $apache::params::apache_name,
   }
 }
