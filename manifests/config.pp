522467a0b6bd97dcfac3dd7c875a538c0a22c0c4
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-02-15 15:09:26 +0000

    Move file resources to apache::config

---
+++ manifests/config.pp
@@ -0,0 +1,39 @@
+# Class apache::config
+class apache::config {
+
+  File {
+    notify => Class['apache::service']
+  }
+
+  file { 'httpd_vdir':
+    ensure  => directory,
+    path    => $apache::params::vdir,
+    recurse => true,
+    purge   => true,
+    notify  => Service['httpd']
+  }
+
+  # Redhat style config
+  if $apache::params::conf_dir and $apache::params::conf_file {
+    file { "${apache::params::conf_dir}/${apache::params::conf_file}":
+      ensure  => present,
+      content => template("apache/${apache::params::conf_file}.erb")
+    }
+    if $default_mods == true {
+      include apache::mod::default
+    }
+  }
+  
+  # Debian style config
+  if $apache::params::mod_dir {
+    file { $apache::params::mod_dir:
+      ensure  => directory,
+      require => Package['httpd'],
+    } -> A2mod <| |>
+    resources { 'a2mod':
+      purge => true,
+    }
+  }
+  
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -28,42 +28,7 @@ class apache (
 
   class { 'apache::params': }
   class { 'apache::install': } ->
+  class { 'apache::config': } ->
   class { 'apache::service }
 
-
-
-  file { 'httpd_vdir':
-    ensure  => directory,
-    path    => $apache::params::vdir,
-    recurse => true,
-    purge   => true,
-    notify  => Service['httpd'],
-    require => Package['httpd'],
-  }
-
-  if $apache::params::conf_dir and $apache::params::conf_file {
-    # Template uses:
-    # - $apache::params::user
-    # - $apache::params::group
-    # - $apache::params::conf_dir
-    # - $serveradmin
-    file { "${apache::params::conf_dir}/${apache::params::conf_file}":
-      ensure  => present,
-      content => template("apache/${apache::params::conf_file}.erb"),
-      notify  => Service['httpd'],
-      require => Package['httpd'],
-    }
-    if $default_mods == true {
-      include apache::mod::default
-    }
-  }
-  if $apache::params::mod_dir {
-    file { $apache::params::mod_dir:
-      ensure  => directory,
-      require => Package['httpd'],
-    } -> A2mod <| |>
-    resources { 'a2mod':
-      purge => true,
-    }
-  }
 }
